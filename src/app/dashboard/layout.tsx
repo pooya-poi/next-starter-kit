d62f2acaf0a14b265fa80f86769fd41c8a299ed5
pooya-poi <pooya585@gmail.com>
2025-01-22 10:56:53 +0330

    add: layout in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,15 @@
-import { ThemeProvider } from "next-themes";
 
-const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <ThemeProvider>
-      <div className="">{children}</div>
+import { ThemeProvider } from 'next-themes';
+import { Metadata } from 'next';
+import CoreProvider from '@/providers/core-provider';
+export const metadata: Metadata = { title: 'داشبورد' };
+const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
+  <ThemeProvider attribute="class" defaultTheme="system">
+    {/* <body className='bg-red-200 dark:bg-slate-200'> */}
+      <CoreProvider>{children}</CoreProvider>
+    {/* </body> */}
   </ThemeProvider>
 );
 
