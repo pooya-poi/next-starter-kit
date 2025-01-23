5329adfa329326c949e31aedb230ecf0a04d8678
pooya-poi <pooya585@gmail.com>
2025-01-23 20:43:24 +0330

    template for testing

--- src/app/dashboard/template.tsx
+++ src/app/dashboard/template.tsx
@@ -1,11 +1,21 @@
 'use client';
-import Sidebar from '@/components/sidebar';
 import ToggleScreen from '@/components/toggle-screen';
 import ToggleTheme from '@/components/toggle-theme';
 import type { ReadonlyChildrenFC } from '@/types/components';
-import { ChevronLeftIcon } from 'lucide-react';
+import { Activity, AirVent, AlarmCheck, ChevronLeftIcon } from 'lucide-react';
 import { useState } from 'react';
 import * as m from 'motion/react-m';
+import Image from 'next/image';
+import Link from 'next/link';
+// import { HomeIcon, SettingsGearIcon } from '@/components/animated-icon/icon';
+import * as Icons from '@/components/animated-icon/index';
+
+const sidebarItems = [
+  { id: 1, name: 'خانه', icon: <Icons.Home /> },
+  { id: 2, name: 'تنظیمات', icon: <Icons.Airplane /> },
+  { id: 3, name: 'پروفایل', icon: '' },
+  { id: 4, name: 'درباره ما', icon: '' },
+];
 
 const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(true);
@@ -17,20 +27,36 @@ const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
             initial={{ width: '350px' }}
             animate={{ width: isExpanded ? '350px' : '150px' }}
             transition={{ type: 'spring', stiffness: 300, damping: 25 }}
-            className="relative hidden h-[80vh] w-72 rounded-lg bg-white md:inline-block"
+            className="relative hidden h-[80vh] w-72 justify-between gap-y-20 rounded-lg bg-white px-5 py-5 md:flex md:flex-col"
           >
-            sidebar
+            <div className="self-center">
+              <Image width={100} height={50} alt="logo" src={'next.svg'} />
+            </div>
+            <div className="flex h-3/4 flex-col gap-y-8 bg-cyan-300">
+              {sidebarItems.map(item => (
+                <Link
+                  href="#"
+                  key={item.id}
+                  className="flex items-center rounded-md bg-slate-400 px-2 py-2"
+                >
+                  {item.icon}
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+            <div className="bg-cyan-300">cta</div>
+
             <m.button
               initial={{ rotate: isExpanded ? '180deg' : '0deg' }}
               animate={{ rotate: isExpanded ? '180deg' : '0deg' }}
               transition={{ type: 'Tween', stiffness: 300, damping: 50 }}
               onClick={() => setIsExpanded(prev => !prev)}
-              className="absolute -left-3 top-20 rounded-full border-4 border-background bg-white"
+              className="absolute -left-4 top-20 rounded-full border-4 border-background bg-white"
             >
               <ChevronLeftIcon size={28} />
             </m.button>
           </m.aside>
-          <div className="flex w-full flex-col ">
+          <div className="flex w-full flex-col">
             <header className="flex h-16 items-center rounded-lg bg-white px-4">
               <ToggleTheme
                 iconSize={10}
@@ -47,10 +73,25 @@ const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
                 rounded={'twoxl'}
               />
             </header>
-            <main className="mt-16 h-[80vh] rounded-lg bg-white">{children}</main>
+            <main className="mt-16 h-[80vh] rounded-lg bg-white">
+              {children}
+
+              {/* <span className='flex'>
+                <Icons.Activity /> Activity
+              </span> */}
+
+              <div className="grid grid-cols-2 gap-4">
+                {Object.entries(Icons).map(([iconName, IconComponent]) => (
+                  <span key={iconName} className="flex items-center gap-2">
+                    <IconComponent className="h-6 w-6" />
+                    {iconName}
+                  </span>
+                ))}
+              </div>
+            </main>
           </div>
         </div>
-        <footer className="h-10 rounded-lg bg-white">footer 2025</footer>
+        {/* <footer className="h-10 rounded-lg bg-white">footer 2025</footer> */}
       </div>
     </>
   );
