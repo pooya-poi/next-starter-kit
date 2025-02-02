39c3500afe9a9f829ea644f5f8c7892303f062ea
pooya-poi <pooya585@gmail.com>
2025-02-02 09:08:38 +0330

    feat: get the structure of the animating and change it later

--- src/components/animated-icon/square-check-big.tsx
+++ src/components/animated-icon/square-check-big.tsx
@@ -3,45 +3,6 @@
 import { useAnimation, type Variants } from 'motion/react';
 import * as m from 'motion/react-m';
 
-
-// const lineVariants: Variants = {
-//   normal: {
-//     rotate: 0,
-//     y: 0,
-//     opacity: 1,
-//   },
-//   animate: (custom: number) => ({
-//     rotate: custom === 1 ? 45 : custom === 3 ? -45 : 0,
-//     y: custom === 1 ? 6 : custom === 3 ? -6 : 0,
-//     opacity: custom === 2 ? 0 : 1,
-//     transition: {
-//       type: 'spring',
-//       stiffness: 260,
-//       damping: 20,
-//     },
-//   }),
-// };
-
-
-
-const lineVariants: Variants = {
-  normal: {
-    rotate: 0,
-    y: 0,
-    opacity: 1,
-  },
-  animate: (custom: number) => ({
-    rotate: custom === 1 ? 45 : custom === 3 ? -45 : 0,
-    y: custom === 1 ? 6 : custom === 3 ? -6 : 0,
-    opacity: custom === 2 ? 0 : 1,
-    transition: {
-      type: 'spring',
-      stiffness: 260,
-      damping: 20,
-    },
-  }),
-};
-
 // const squareVariant: Variants = {
 //   from: {
 //     opacity: 0,
@@ -66,6 +27,27 @@ const lineVariants: Variants = {
 //     },
 //   },
 // };
+const svgVariants = {
+  hidden: { rotate: -180 },
+  visible: {
+    rotate:0,
+    transition:{duration:1}
+  }
+};
+const pathVariants = {
+  hidden:{
+    opacity:10,
+    pathLength:0
+  },
+  visible:{
+    opacity:1,
+    pathLength:1,
+    transition:{
+      duration:2,
+      ease:"easeInOut"
+    }
+  }
+};
 
 const SquareCheckBigIcon = ({ className }: { className?: string }) => {
   const controls = useAnimation();
@@ -73,51 +55,13 @@ const SquareCheckBigIcon = ({ className }: { className?: string }) => {
   return (
     <div
       className={`flex cursor-pointer select-none items-center justify-center rounded-md p-2 transition-colors duration-200 ${className}`}
-    //   onMouseEnter={() => controls.start('animate')}
-    //   onMouseLeave={() => controls.start('normal')}
-      onMouseEnter={() => controls.start('animate')}
-      onMouseLeave={() => controls.start('normal')}
+        onMouseEnter={() => controls.start('visible')}
+        onMouseLeave={() => controls.start('hidden')}
+      // onMouseEnter={() => console.log('icon is logging')}
+      // onMouseLeave={() => console.log('icon is logging')}
     >
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="28"
-        height="28"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <m.line
-          x1="4"
-          y1="6"
-          x2="20"
-          y2="6"
-          variants={lineVariants}
-          animate={controls}
-          custom={1}
-        />
-        <m.line
-          x1="4"
-          y1="12"
-          x2="20"
-          y2="12"
-          variants={lineVariants}
-          animate={controls}
-          custom={2}
-        />
-        <m.line
-          x1="4"
-          y1="18"
-          x2="20"
-          y2="18"
-          variants={lineVariants}
-          animate={controls}
-          custom={3}
-        />
-      </svg> */}
-      <svg
+     
+      <m.svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
@@ -128,20 +72,21 @@ const SquareCheckBigIcon = ({ className }: { className?: string }) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         // class="lucide lucide-square-check-big"
+        variants={svgVariants}
       >
         <m.path
-          initial="from"
-          animate="to"
-          variants={lineVariants}
+          initial="hidden"
+          animate={controls}
+          variants={pathVariants}
           d="M21 10.5V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.5"
         />
         <m.path
-          initial="from"
-          animate="to"
-          variants={lineVariants}
+          initial="hidden"
+          animate={controls}
+          variants={pathVariants}
           d="m9 11 3 3L22 4"
         />
-      </svg>
+      </m.svg>
     </div>
   );
 };
