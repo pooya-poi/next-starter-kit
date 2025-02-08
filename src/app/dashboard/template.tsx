9b4e884cbf9bc51f8c5ad39ed4624c53dab3f82b
pooya-poi <pooya585@gmail.com>
2025-02-09 01:05:41 +0330

    feat: refactor DashboardTemplate to use Header and Footer components, update sidebar icons and layout

--- src/app/dashboard/template.tsx
+++ src/app/dashboard/template.tsx
@@ -1,42 +1,36 @@
 'use client';
-import ToggleScreen from '@/components/toggle-screen';
-import ToggleTheme from '@/components/toggle-theme';
-import MobileSidebar from '@/components/MobileSidebar';
+
 import type { ReadonlyChildrenFC } from '@/types/components';
-import { Bell, ChevronLeftIcon, MessageSquareMore, Search } from 'lucide-react';
-import { useEffect, useState, useId } from 'react';
+import { ChevronLeftIcon } from 'lucide-react';
+import { useState } from 'react';
 import * as m from 'motion/react-m';
 import Image from 'next/image';
 import Link from 'next/link';
-import * as Icons from '@/components/animated-icon/index';
-
-import { useMobileSidebar } from '@/hooks/useMobileSidebar';
-import ToggleSidebar from '@/components/toggle-sidebar';
-
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Badge } from '@/components/ui/badge';
-
 import { usePathname } from 'next/navigation';
+
+import HomeIcon from '@/components/svg-icon/home-icon';
+
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import { Github } from '@/components/animated-icon';
 
 const sidebarItems = [
   {
     id: 1,
     name: 'داشبورد',
-    icon: <Icons.Home className="size-10" />,
+    icon: <HomeIcon className="size-7" />,
     path: '/dashboard',
   },
   {
     id: 2,
     name: 'آیکون‌ها',
-    icon: <Icons.Airplane className="size-10" />,
+    // icon: <Icons.Airplane className="size-10" />,
     path: '/dashboard/icons',
   },
   {
     id: 3,
     name: 'دسته‌بندی',
-    icon: <Icons.Cart className="size-10" />,
+    // icon: <Icons.Cart className="size-10" />,
     path: '',
   },
   { id: 4, name: 'موجودی', icon: '', path: '' },
@@ -55,20 +49,8 @@ const sidebarItems = [
 
 const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(true);
-  const { isOpen, openSidebar, closeSidebar } = useMobileSidebar();
-  const [offset, setOffset] = useState(0);
-  const id = useId();
   const pathname = usePathname();
   const currentRoute = pathname;
- const currentYear = new Date().toLocaleDateString('fa-IR', { year: 'numeric'});
- 
-
-  useEffect(() => {
-    window.onscroll = () => {
-      setOffset(window.scrollY);
-    };
-  }, []);
-  console.log('offset', offset);
 
   const breadcrumbs: { [key: string]: string } = {
     '/dashboard': 'داشبورد',
@@ -97,30 +79,37 @@ const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
         <div className="flex gap-x-4">
           {/* Desktop sidebar */}
           <m.aside
-            initial={{ width: '300px' }}
-            animate={{ width: isExpanded ? '300px' : '90px' }}
+            initial={{ width: '260px' }}
+            animate={{ width: isExpanded ? '260px' : '90px' }}
             transition={{ type: 'spring', stiffness: 300, damping: 25 }}
-            className="sticky top-5 hidden h-[95vh] w-72 justify-between gap-y-20 rounded-lg bg-white px-5 py-5 dark:bg-slate-800 md:flex md:flex-col"
+            className="sticky top-5 hidden h-[95vh] w-72 justify-between gap-y-20 rounded-lg bg-white px-2 py-5 dark:bg-slate-800 md:flex md:flex-col"
           >
             <div className="self-center">
               <Image width={100} height={50} alt="logo" src="/next.svg" />
             </div>
-            {/* <div className="flex h-3/4 flex-col gap-y-8 overflow-y-auto"> */}
-            {/* <div className="flex h-3/4 flex-col gap-y-8 scrollable-y-content"> */}
-            <div className="scrollbar-fix flex h-3/4 flex-col gap-y-8 overflow-y-auto pr-3">
+            <div
+              className={`scrollbar-fix flex h-3/4 flex-col gap-y-8 overflow-y-auto ${isExpanded ? '' : 'items-center'} `}
+            >
               {sidebarItems.map(item => (
                 <Link
                   href={item.path}
                   key={item.id}
-                  // className={ `flex items-center rounded-[14]  pr-1 py-1 text-white bg-slate-800`}
-                  className={`flex items-center rounded-lg ${!isExpanded ? 'w-10' : ''} ${currentRoute === item.path ? 'bg-slate-800 text-white dark:bg-white dark:text-slate-900' : 'hover:bg-slate-200'}`}
+                  className={`flex items-center ${!isExpanded ? 'size-12 justify-center rounded-xl p-2' : 'rounded-xl px-4 py-2'} ${currentRoute === item.path ? 'bg-slate-800 text-white dark:bg-white dark:text-slate-900' : 'hover:bg-slate-200'}`}
                 >
                   {item.icon}
                   {isExpanded && item.name}
                 </Link>
               ))}
             </div>
-            <div className="bg-cyan-300">cta</div>
+            <div className="flex items-center justify-center">
+              <Link
+                target="_blank"
+                className="flex items-center"
+                href="https://github.com/pooya-poi/next-starter-kit/"
+              >
+                <Github className="size-6" />
+              </Link>{' '}
+            </div>
             <m.button
               initial={{ rotate: isExpanded ? '180deg' : '0deg' }}
               animate={{ rotate: isExpanded ? '180deg' : '0deg' }}
@@ -132,79 +121,11 @@ const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
             </m.button>
           </m.aside>
 
-          {/* Mobile Sidebar */}
-          <MobileSidebar isOpen={isOpen} closeSidebar={closeSidebar} />
-
           <div className="flex w-full flex-col gap-y-3">
-            <header
-              onScroll={() => console.log('header is scrolling')}
-              className={` ${offset > 120 ? 'sticky top-5 shadow-2xl transition-all ease-in-out' : ''} flex h-16 items-center justify-between rounded-lg bg-white/70 px-4 backdrop-blur-sm dark:bg-slate-800/80`}
-            >
-              {/* rightside */}
-              <div>
-                {/* search */}
-                <div className="relative">
-                  <Input
-                    id={id}
-                    className="peer pr-8"
-                    placeholder="جستجو"
-                    type="search"
-                  />
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center justify-center pe-3 pr-3 text-muted-foreground/80 peer-disabled:opacity-50">
-                    <Search size={16} strokeWidth={2} aria-hidden="true" />
-                  </div>
-                </div>
-              </div>
-              {/* leftside */}
-              <div className="flex items-center gap-x-10">
-                {/* toggles */}
-                <div className="flex flex-row-reverse gap-x-4">
-                  <ToggleTheme
-                    iconSize={10}
-                    className="md:flex"
-                    size={'lg'}
-                    variant={'default'}
-                    rounded={'2xl'}
-                  />
-                  <ToggleScreen
-                    className="md:flex"
-                    size={'lg'}
-                    variant={'default'}
-                    rounded={'twoxl'}
-                  />
-                </div>
-                {/* notifs */}
-                <div className="flex gap-x-4">
-                  <button>
-                    <Icons.MessageCircleMore className="size-12 rounded-2xl bg-background p-2" />
-                  </button>
-                  <button className="relative">
-                    <Icons.Bell className="size-12 rounded-2xl bg-background p-2" />
-                    <Badge className="absolute -top-2 left-full flex max-w-8 -translate-x-6 items-center rounded-full border-background px-2">
-                      55
-                    </Badge>
-                  </button>
-                </div>
-                {/* avatar */}
-                <Avatar>
-                  <AvatarImage
-                    src="https://avatar.iran.liara.run/public/24"
-                    alt="Kelly King"
-                  />
-                  <AvatarFallback>KK</AvatarFallback>
-                </Avatar>
-              </div>
-
-              <ToggleSidebar
-                openSidebar={openSidebar}
-                size={'lg'}
-                rounded={'2xl'}
-                variant={'default'}
-              />
-            </header>
+            <Header />
             {/* BreadCrumb */}
             <nav className="px-5 font-extrabold">
-              <ul className="flex gap-2">
+              <ul className="flex gap-2 text-xs">
                 <li>
                   <Link href="/dashboard" className="text-blue-600">
                     خانه
@@ -212,7 +133,7 @@ const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
                 </li>
                 {breadcrumbItems.map((item, index) => (
                   <li key={item.path} className="flex items-center">
-                    <ChevronLeftIcon size={16} className="mx-2" />
+                    <ChevronLeftIcon size={16} className="mx-1" />
                     {index === breadcrumbItems.length - 1 ? (
                       <span>{item.name}</span>
                     ) : (
@@ -227,23 +148,7 @@ const DashboardTemplate: ReadonlyChildrenFC = ({ children }) => {
             <main className="rounded-lg bg-white p-6 dark:bg-slate-800">
               {children}
             </main>
-            <footer className="flex justify-between items-center rounded-lg bg-white px-5 py-2 text-xs font-light text-gray-500 dark:bg-slate-800">
-              <p>حقوق این سایت متعلق به شرکت فلان می‌باشد &copy;  {currentYear}</p>
-              <span className="flex flex-row-reverse items-center gap-x-4">
-                <Link href="/instagram" className="">
-                  <Icons.Instagram className="size-5 p-0 text-gray-300 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-300" />
-                </Link>
-                <Link href="/instagram" className="">
-                  <Icons.Facebook className="size-5 p-0 text-gray-300 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-300" />
-                </Link>
-                <Link href="/instagram" className="">
-                  <Icons.Instagram className="size-5 p-0 text-gray-300 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-300" />
-                </Link>
-                <Link href="/instagram" className="">
-                  <Icons.Telegram className="size-5 p-0 text-gray-300 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-300" />
-                </Link>
-              </span>
-            </footer>
+            <Footer />
           </div>
         </div>
       </div>
