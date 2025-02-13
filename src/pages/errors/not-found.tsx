36f902bda0b4efa97055accab54b4aefbb45cd4c
pooya-poi <pooya585@gmail.com>
2025-02-13 12:58:06 +0330

    feat: enhance error handling pages with dynamic status codes and improved layout

--- src/pages/errors/not-found.tsx
+++ src/pages/errors/not-found.tsx
@@ -2,16 +2,24 @@
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 
-export default function NotFound() {
+
+
+// export default function NotFound() {
+const NotFound = () => {
   const router = useRouter();
+
   return (
     <main className="flex h-screen items-center justify-center overflow-hidden bg-background">
-      <div className="flex h-full w-full flex-col items-center justify-center gap-5 rounded-2xl bg-white p-5 py-10 text-gray-800 shadow-2xl shadow-slate-400/50 lg:h-1/2 lg:w-2/4">
+      <div className="flex h-full w-full  flex-col items-center justify-center gap-5 rounded-2xl bg-white p-5 py-10 text-gray-800 shadow-2xl shadow-slate-400/50 lg:h-2/3 lg:w-3/4">
         {/* Title Section */}
         {/* <div className="mb-8"> */}
         <div className="flex flex-col items-center">
-          <span className="text-4xl md:text-7xl font-bold">乁⁠(⁠ ⁠•⁠_⁠•⁠ ⁠)⁠ㄏ</span>
-          <h1 className="text-center text-6xl md:text-9xl font-black">Error 404 </h1>
+          <span className="text-4xl font-bold md:text-7xl">
+            乁⁠(⁠ ⁠•⁠_⁠•⁠ ⁠)⁠ㄏ
+          </span>
+          <h1 className="text-center text-6xl font-black md:text-9xl">
+            Error 404
+          </h1>
         </div>
 
         <p className="mt-4 text-center text-2xl">Oops! Page not found.</p>
@@ -22,7 +30,6 @@ export default function NotFound() {
             The page you're looking for doesn't exist. Let's get you back on
             track!
           </p>
-
           <Button
             onClick={() => router.back()}
             className="inline-block rounded bg-blue-500 px-6 py-3 font-semibold text-white transition duration-200 hover:bg-blue-600"
@@ -33,4 +40,6 @@ export default function NotFound() {
       </div>
     </main>
   );
-}
+};
+
+export default appWithTranslation(NotFound);
