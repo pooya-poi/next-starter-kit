49405512a07cd36b1a136dd4aca0bed20b09cf68
pooya-poi <pooya585@gmail.com>
2025-05-02 12:50:58 +0330

    deleted file du to rename

--- src/app/new/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="rounded-lg flex flex-col gap-y-4">
-      <div className="bg-background h-28 rounded-lg p-5">item</div>
-      <div className="bg-background h-20 rounded-lg p-5">item</div>
-      <div className="bg-background h-40 rounded-lg p-5">item</div>
-    </div>
-  );
-}
