fd0b558c0006add581528d382b513bbd20e543a1
pooya-poi <pooya585@gmail.com>
2025-05-09 01:55:17 +0330

    chore: fix layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,27 +1,31 @@
-'use client';
 import Footer from '@/layout/footer';
 import Header from '@/layout/header';
-import Sidebar from '@/layout/sidebar';
+import { AppSidebar } from '@/layout/app-sidebar';
 
 import BreadCrumb from '@/layout/breadcrumb';
 
-export default function AdminLayout({
+import { SidebarInset, SidebarProvider } from '@/components/sidebar';
+import { cookies } from 'next/headers';
+
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="container m-auto flex max-w-[1920px] flex-col gap-3 p-1 md:p-3">
-      <div className="flex gap-x-5">
-        <Sidebar />
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true';
 
-        <div className="flex w-full flex-col gap-y-4">
+  return (
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AppSidebar />
+      <SidebarInset className="bg-muted">
+        <div className="flex w-full flex-col gap-y-4 p-2">
           <Header />
           <BreadCrumb />
           <main>{children}</main>
         </div>
-      </div>
-      <Footer />
-    </div>
+        <Footer />
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
