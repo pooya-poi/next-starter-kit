37ef2e2f68d200c5ef5036f5fc2a867c5e466a23
pooya-poi <pooya585@gmail.com>
2025-05-11 15:47:54 +0330

    chore: enhanced layout viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  interactiveWidget: 'resizes-content',
+  userScalable: true,
   themeColor: [
     { media: '(prefers-color-scheme: dark)', color: '#0e0e0e' },
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
