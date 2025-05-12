704bfbc64de28cac68152d24b3fa40a8f410301a
pooya-poi <pooya585@gmail.com>
2025-05-13 01:54:16 +0330

    refactor: enhance DotIndicator component with active state variants

--- src/components/dot-indicator.tsx
+++ src/components/dot-indicator.tsx
@@ -1,7 +1,21 @@
-const DotIndicator: React.FC = () => (
+import { cva } from 'class-variance-authority';
+
+const dotVariants = cva('relative inline-flex size-1.5 rounded-full', {
+  variants: {
+    active: {
+      true: 'bg-primary ',
+      false: 'bg-muted ',
+    },
+  },
+});
+interface DotIndicatorProps {
+  active?: boolean;
+}
+const DotIndicator: React.FC<DotIndicatorProps> = ({active}) => (
   <span className="absolute top-1/2 -right-2 flex size-2 -translate-y-1/2">
     <span className="bg-background absolute inline-flex h-full w-full animate-ping rounded-full opacity-75" />
-    <span className="bg-primary relative inline-flex size-1.5 rounded-full" />
+    {/* <span className="bg-primary relative inline-flex size-1.5 rounded-full" /> */}
+    <span className={dotVariants({active:active})} />
   </span>
 );
 
